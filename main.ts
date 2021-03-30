9683e49049fcce1c3ac01ba643644baca30ef916
karamage <karamage.umauma@gmail.com>
2021-03-30 13:38:35 +0900

    first

---
+++ main.ts
@@ -0,0 +1,25 @@
+const fortunes = [
+  '大吉🎉',
+  '吉🎯',
+  '凶🌧',
+  '大凶😈'
+] as const
+  
+addEventListener('fetch', (event) => {
+  const fortune = fortunes[Math.floor(Math.random() * fortunes.length)]
+  const html = `
+    <html>
+      <head>
+        <title>Omikuji</title>
+        <meta charset="utf-8" />
+      </head>
+      <body>
+        <h1>${fortune}</h1>
+      </body>
+    </html>
+    `
+  const response = new Response(html, {
+    headers: { "content-type": "text/html; charset=UTF-8" },
+  });
+  event.respondWith(response);
+})
